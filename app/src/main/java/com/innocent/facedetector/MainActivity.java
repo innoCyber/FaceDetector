619151f0f008fbda69b1aa9cf667d8c7ee3a9892
innoCyber <innocentileka@gmail.com>
2017-12-13 10:40:14 +0100

    Initial commit

---
+++ app/src/main/java/com/innocent/facedetector/MainActivity.java
@@ -0,0 +1,84 @@
+package com.innocent.facedetector;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button detectButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
+        detectButton = (Button)findViewById(R.id.detectButton);
+
+        final Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.gdgeventt);
+        imageView.setImageBitmap(myBitmap);
+
+
+        final Paint facePaint = new Paint();
+        facePaint.setStrokeWidth(10);
+        facePaint.setColor(Color.WHITE);
+        facePaint.setStyle(Paint.Style.STROKE);
+
+        final Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(),myBitmap.getHeight(), Bitmap.Config.RGB_565);
+        final Canvas canvas = new Canvas(tempBitmap);
+        canvas.drawBitmap(myBitmap,0,0,null);
+
+        detectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FaceDetector faceDetector = new FaceDetector.Builder(getApplicationContext())
+                        .setTrackingEnabled(false)
+                        .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                        .setMode(FaceDetector.FAST_MODE)
+                        .build();
+                if(!faceDetector.isOperational())
+                {
+                    Toast.makeText(MainActivity.this, "Face Detector components are still downloading, please relaunch", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                SparseArray<Face> sparseArray = faceDetector.detect(frame);
+
+                for(int i=0;i<sparseArray.size();i++)
+                {
+                    Face face = sparseArray.valueAt(i);
+                    float x1=face.getPosition().x;
+                    float y1 =face.getPosition().y;
+                    float x2 = x1+face.getWidth();
+                    float y2=y1+face.getHeight();
+                    RectF rectF = new RectF(x1,y1,x2,y2);
+                    canvas.drawRoundRect(rectF,2,2,facePaint);
+
+                }
+
+                imageView.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
+
+            }
+        });
+
+
+    }
+}
+
